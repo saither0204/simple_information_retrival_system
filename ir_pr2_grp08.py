821f8aa30e79937b878864c9f1a48fccedff606c
saither0204 <sarthakshah0204@gmail.com>
2023-06-26 22:40:28 +0200

    v3.3.1

--- ir_pr2_grp08.py
+++ ir_pr2_grp08.py
@@ -7,6 +7,408 @@ from collections import defaultdict
 from porterStemmer import PorterStemmer
 
 
+class PorterStemmer:
+    def isCons(self, alphabet):#function checking if a letter is a consonant or not and returns a boolean value (true or false).
+        if alphabet == 'a' or alphabet == 'e' or alphabet == 'i' or alphabet == 'o' or alphabet == 'u':
+            return False
+        else:
+            return True
+        
+
+    def isConsonant(self, word, i):#function that returns true only if the letter at i th position 
+        #in the argument 'word' is a consonant.But if the letter is 'y' and the letter at i-1 th position 
+        #is also a consonant, then it returns false.
+        alphabet = word[i]
+        if self.isCons(alphabet):
+            if alphabet == 'y' and self.isCons(word[i-1]):
+                return False
+            else:
+                return True
+        else:
+            return False
+        
+
+    def isVowel(self, word, i):#function that returns true if the letter at i th position in the argument 'word'
+        #is a vowel
+        variable = self.isConsonant(word, i)
+        return not(variable)
+
+    # *S  - the stem ends with S (and similarly for the other letters). 
+    def endsWith(self, stem, alphabet):#returns true if the word 'stem' ends with 'alphabet' 
+        if stem.endswith(alphabet):
+            return True
+        else:
+            return False
+
+    # *v* - the stem contains a vowel. 
+    def containsVowel(self, stem):#returns true if the word 'stem' contains a vowel
+        for i in stem:
+            if not self.isCons(i):
+                return True
+        return False
+
+    # *d  - the stem ends with a double consonant (e.g. -TT, -SS).
+    def doubleCons(self, stem):#returns true if the word 'stem' ends with 2 consonants
+        if len(stem) >= 2:
+            if self.isConsonant(stem, -1) and self.isConsonant(stem, -2):
+                return True
+            else:
+                return False
+        else:
+            return False
+    
+    # *o  - the stem ends cvc, where the second c is not W, X or Y (e.g. -WIL, -HOP).
+    def cvc(self, word):
+        #returns true if the last 3 letters of the word are of the following pattern: consonant,vowel,consonant
+        #but if the last word is either 'w','x' or 'y', it returns false
+        if len(word) >= 3:
+            f = -3
+            s = -2
+            t = -1
+            third = word[t]
+            if self.isConsonant(word, f) and self.isVowel(word, s) and self.isConsonant(word, t):
+                if third != 'w' and third != 'x' and third != 'y':
+                    return True
+                else:
+                    return False
+            else:
+                return False
+        else:
+            return False
+
+    def getForm(self, word):
+        #This function takes a word as an input, and checks for vowel and consonant sequences in that word.
+        #vowel sequence is denoted by V and consonant sequences by C
+        #For example, the word 'balloon' can be divived into following sequences:
+        #'b' : C
+        #'a' : V
+        #'ll': C
+        #'oo': V
+        #'n' : C
+        #So form = [C,V,C,V,C] and formstr = CVCVC
+        form = []
+        formStr = ''
+        for i in range(len(word)):
+            if self.isConsonant(word, i):
+                if i != 0:
+                    prev = form[-1]
+                    if prev != 'C':
+                        form.append('C')
+                else:
+                    form.append('C')
+            else:
+                if i != 0:
+                    prev = form[-1]
+                    if prev != 'V':
+                        form.append('V')
+                else:
+                    form.append('V')
+        for j in form:
+            formStr += j
+        return formStr
+
+    def getM(self, word):
+        #returns value of M which is equal to number of 'VC' in formstr
+        #So in above example of word 'balloon', we have 2 'VC'
+
+        form = self.getForm(word)
+        m = form.count('VC')
+        return m
+
+    
+    def replace(self, orig, rem, rep):
+        #this function checks if string 'orig' ends with 'rem' and
+        #replaces 'rem' by the substring 'rep'. The resulting string 'replaced'
+        #is returned.
+
+        result = orig.rfind(rem)
+        base = orig[:result]
+        replaced = base + rep
+        return replaced
+
+    def replaceM0(self, orig, rem, rep):
+        #same as the function replace(), except that it checks the value of M for the 
+        #base string. If it is >0 , it replaces 'rem' by 'rep', otherwise it returns the
+        #original string
+
+        result = orig.rfind(rem)
+        base = orig[:result]
+        if self.getM(base) > 0:
+            replaced = base + rep
+            return replaced
+        else:
+            return orig
+
+    def replaceM1(self, orig, rem, rep):
+        #same as replaceM0(), except that it replaces 'rem' by 'rep', only when M>1 for
+        #the base string
+
+        result = orig.rfind(rem)
+        base = orig[:result]
+        if self.getM(base) > 1:
+            replaced = base + rep
+            return replaced
+        else:
+            return orig
+
+    def step1a(self, word):
+        #In a given word, this function replaces 'sses' by 'ss', 'ies' by 'i',
+        #'ss' by 'ss' and 's' by ''
+
+        """step1a() gets rid of plurals. e.g.
+
+           caresses  ->  caress
+           ponies    ->  poni
+           ties      ->  ti
+           caress    ->  caress
+           cats      ->  cat
+        """
+        if word.endswith('sses'):
+            word = self.replace(word, 'sses', 'ss')
+        elif word.endswith('ies'):
+            word = self.replace(word, 'ies', 'i')
+        elif word.endswith('ss'):
+            word = self.replace(word, 'ss', 'ss')
+        elif word.endswith('s'):
+            word = self.replace(word, 's', '')
+        else:
+            pass
+        return word
+
+    def step1b(self, word):
+        #this function checks if a word ends with 'eed','ed' or 'ing' and replces these substrings by
+        #'ee','' and ''. If after the replacements in case of 'ed' and 'ing', the resulting word
+        # -> ends with 'at','bl' or 'iz' : add 'e' to the end of the word
+        # -> ends with 2 consonants and its last letter isn't 'l','s' or 'z': remove last letter of the word
+        # -> has 1 as value of M and the cvc(word) returns true : add 'e' to the end of the word
+        
+        '''
+        step1b gets rid of -eed -ed or -ing. e.g.
+
+        feed      ->  feed
+           agreed    ->  agree
+           disabled  ->  disable
+
+           matting   ->  mat
+           mating    ->  mate
+           meeting   ->  meet
+           milling   ->  mill
+           messing   ->  mess
+
+           meetings  ->  meet
+        '''
+        flag = False
+        if word.endswith('eed'):
+            result = word.rfind('eed')
+            base = word[:result]
+            if self.getM(base) > 0:
+                word = base
+                word += 'ee'
+        elif word.endswith('ed'):
+            result = word.rfind('ed')
+            base = word[:result]
+            if self.containsVowel(base):
+                word = base
+                flag = True
+        elif word.endswith('ing'):
+            result = word.rfind('ing')
+            base = word[:result]
+            if self.containsVowel(base):
+                word = base
+                flag = True
+        if flag:
+            if word.endswith('at') or word.endswith('bl') or word.endswith('iz'):
+                word += 'e'
+            elif self.doubleCons(word) and not self.endsWith(word, 'l') and not self.endsWith(word, 's') and not self.endsWith(word, 'z'):
+                word = word[:-1]
+            elif self.getM(word) == 1 and self.cvc(word):
+                word += 'e'
+            else:
+                pass
+        else:
+            pass
+        return word
+
+    def step1c(self, word):
+        #In words ending with 'y' this function replaces 'y' by 'i'
+        
+        """step1c() turns terminal y to i when there is another vowel in the stem."""
+
+        if word.endswith('y'):
+            result = word.rfind('y')
+            base = word[:result]
+            if self.containsVowel(base):
+                word = base
+                word += 'i'
+        return word
+
+    def step2(self, word):
+        #this function checks the value of M, and replaces the suffixes accordingly
+        
+        """step2() maps double suffices to single ones.
+        so -ization ( = -ize plus -ation) maps to -ize etc. note that the
+        string before the suffix must give m() > 0.
+        """
+
+        if word.endswith('ational'):
+            word = self.replaceM0(word, 'ational', 'ate')
+        elif word.endswith('tional'):
+            word = self.replaceM0(word, 'tional', 'tion')
+        elif word.endswith('enci'):
+            word = self.replaceM0(word, 'enci', 'ence')
+        elif word.endswith('anci'):
+            word = self.replaceM0(word, 'anci', 'ance')
+        elif word.endswith('izer'):
+            word = self.replaceM0(word, 'izer', 'ize')
+        elif word.endswith('abli'):
+            word = self.replaceM0(word, 'abli', 'able')
+        elif word.endswith('alli'):
+            word = self.replaceM0(word, 'alli', 'al')
+        elif word.endswith('entli'):
+            word = self.replaceM0(word, 'entli', 'ent')
+        elif word.endswith('eli'):
+            word = self.replaceM0(word, 'eli', 'e')
+        elif word.endswith('ousli'):
+            word = self.replaceM0(word, 'ousli', 'ous')
+        elif word.endswith('ization'):
+            word = self.replaceM0(word, 'ization', 'ize')
+        elif word.endswith('ation'):
+            word = self.replaceM0(word, 'ation', 'ate')
+        elif word.endswith('ator'):
+            word = self.replaceM0(word, 'ator', 'ate')
+        elif word.endswith('alism'):
+            word = self.replaceM0(word, 'alism', 'al')
+        elif word.endswith('iveness'):
+            word = self.replaceM0(word, 'iveness', 'ive')
+        elif word.endswith('fulness'):
+            word = self.replaceM0(word, 'fulness', 'ful')
+        elif word.endswith('ousness'):
+            word = self.replaceM0(word, 'ousness', 'ous')
+        elif word.endswith('aliti'):
+            word = self.replaceM0(word, 'aliti', 'al')
+        elif word.endswith('iviti'):
+            word = self.replaceM0(word, 'iviti', 'ive')
+        elif word.endswith('biliti'):
+            word = self.replaceM0(word, 'biliti', 'ble')
+        return word
+
+    def step3(self, word):
+        #this function checks the value of M, and replaces the suffixes accordingly
+        
+        """step3() dels with -ic-, -full, -ness etc. similar strategy to step2."""
+
+        if word.endswith('icate'):
+            word = self.replaceM0(word, 'icate', 'ic')
+        elif word.endswith('ative'):
+            word = self.replaceM0(word, 'ative', '')
+        elif word.endswith('alize'):
+            word = self.replaceM0(word, 'alize', 'al')
+        elif word.endswith('iciti'):
+            word = self.replaceM0(word, 'iciti', 'ic')
+        elif word.endswith('ful'):
+            word = self.replaceM0(word, 'ful', '')
+        elif word.endswith('ness'):
+            word = self.replaceM0(word, 'ness', '')
+        return word
+
+    def step4(self, word):
+        #this function checks the value of M, and replaces the suffixes accordingly
+        
+        """step4() takes off -ant, -ence etc., in context <c>vcvc<v>{meaning, M >1 for the word}."""
+
+        if word.endswith('al'):
+            word = self.replaceM1(word, 'al', '')
+        elif word.endswith('ance'):
+            word = self.replaceM1(word, 'ance', '')
+        elif word.endswith('ence'):
+            word = self.replaceM1(word, 'ence', '')
+        elif word.endswith('er'):
+            word = self.replaceM1(word, 'er', '')
+        elif word.endswith('ic'):
+            word = self.replaceM1(word, 'ic', '')
+        elif word.endswith('able'):
+            word = self.replaceM1(word, 'able', '')
+        elif word.endswith('ible'):
+            word = self.replaceM1(word, 'ible', '')
+        elif word.endswith('ant'):
+            word = self.replaceM1(word, 'ant', '')
+        elif word.endswith('ement'):
+            word = self.replaceM1(word, 'ement', '')
+        elif word.endswith('ment'):
+            word = self.replaceM1(word, 'ment', '')
+        elif word.endswith('ent'):
+            word = self.replaceM1(word, 'ent', '')
+        elif word.endswith('ou'):
+            word = self.replaceM1(word, 'ou', '')
+        elif word.endswith('ism'):
+            word = self.replaceM1(word, 'ism', '')
+        elif word.endswith('ate'):
+            word = self.replaceM1(word, 'ate', '')
+        elif word.endswith('iti'):
+            word = self.replaceM1(word, 'iti', '')
+        elif word.endswith('ous'):
+            word = self.replaceM1(word, 'ous', '')
+        elif word.endswith('ive'):
+            word = self.replaceM1(word, 'ive', '')
+        elif word.endswith('ize'):
+            word = self.replaceM1(word, 'ize', '')
+        elif word.endswith('ion'):
+            result = word.rfind('ion')
+            base = word[:result]
+            if self.getM(base) > 1 and (self.endsWith(base, 's') or self.endsWith(base, 't')):
+                word = base
+            word = self.replaceM1(word, '', '')
+        return word
+
+    def step5a(self, word):
+        #this function checks if the word ends with 'e'. If it does, it checks the value of
+        #M for the base word. If M>1, OR, If M = 1 and cvc(base) is false, it simply removes 'e'
+        #ending.
+        
+        """step5() removes a final -e if m() > 1.
+        """
+
+        if word.endswith('e'):
+            base = word[:-1]
+            if self.getM(base) > 1:
+                word = base
+            elif self.getM(base) == 1 and not self.cvc(base):
+                word = base
+        return word
+
+    def step5b(self, word):
+        #this function checks if the value of M for the word is greater than 1 and it ends with 2 consonants
+        # and it ends with 'l', it removes 'l'
+        
+        #step5b changes -ll to -l if m() > 1
+        if self.getM(word) > 1 and self.doubleCons(word) and self.endsWith(word, 'l'):
+            word = word[:-1]
+        return word
+
+    def stem(self, word):
+        """
+        I read the porter.txt file and found your note.
+        """
+        #steps to be followed for stemming according to the porter stemmer :)
+
+        word = self.step1a(word)
+        word = self.step1b(word)
+        word = self.step1c(word)
+        word = self.step2(word)
+        word = self.step3(word)
+        word = self.step4(word)
+        word = self.step5a(word)
+        word = self.step5b(word)
+        return word
+    
+
+
+    
+
+
+
+
+
 def precision(query, dict_gt, inverted_index):
     temp_tot_relevant_docs = inverted_index[query]
     tot_relevant_docs = []
@@ -278,8 +680,7 @@ def linear_search_collection(chapter_titles, dict_titles_texts_original, dict_ti
         
     else:
         if stemming is True:
-            #obj_word.stem(query)
-            #print(obj_word.stem(query)) #code to test of function works or not.
+
             if documents == 'original':
                 var1 = linear_search_stemmed_form_original(dict_titles_texts_original = dict_titles_texts_original, query = query, obj_word = obj_word)
                 for i in var1:
@@ -414,7 +815,7 @@ if __name__ == '__main__':
     chapters_text_values = chapters_text
     for i in range(1,len(chapters_list_final)+1):
         chapter_list_final_keys.append(create_file_names_with_underscore(chapters_list_final[i-1],str(i).zfill(2)))
-    #print(chapter_list_with_required_titles[0:5])
+
     
     dict_titles_texts_original = dict(zip(chapter_list_final_keys, chapters_text_values))
     
@@ -441,20 +842,19 @@ if __name__ == '__main__':
         elif args.search_mode == 'inverted':
             unique = []
             for text in dict_titles_texts_st.values():
-                #print(text)
+
                 text = text.lower()
                 text = cleaning_text(text)
                 words = text.split(" ")
                 for word in words:
-                #print(word)
+
                     if word not in unique:
                         unique.append(word)
 
             unique.remove('')
             unique.sort()
-            # print(unique)
-            # print(len(unique))
-            
+
+
             inverted_index = defaultdict(list)
             for i in range(0,len(unique)):
                 for key,value in dict_titles_texts_st.items():
@@ -467,7 +867,7 @@ if __name__ == '__main__':
             start = time.time()
             inverted_list_search(chapter_list_final_keys, inverted_index, query=args.query, model=args.model, documents=args.documents, stemming=args.stemming)
             end = time.time()
-            # print(f'T={(end - start)*1000} ms')
+            
             try:
                 ground_truth_path = os.path.abspath("ground_truth.txt")
                 with open(ground_truth_path, 'r') as gt:
@@ -495,23 +895,3 @@ if __name__ == '__main__':
                 print(f'T={(end - start)*1000} ms, P=?,R=?')         
             
         
-    # ground_truth_path = os.path.abspath("ground_truth.txt")
-    # with open(ground_truth_path, 'r') as gt:
-    #     terms = gt.read()
-    #     terms = terms.split('\n')
-    #     var = []
-    #     for i in terms:
-    #         temp = i.split(' - ')
-    #         var.append(temp)
-    #     var = var[:6]
-
-    # gt_keys = []
-    # gt_values = []
-    # for i in range(len(var)):
-    #     gt_values.append((var[i][1]).split(","))
-    #     gt_keys.append(var[i][0])
-
-    # dict_gt = dict(zip(gt_keys,gt_values))
-
-    # recall('animal', dict_gt, inverted_index)
-    # precision('animal', dict_gt,inverted_index)
